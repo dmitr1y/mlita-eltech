019bdde72b93f5fc7ec194fc1e0a2fe468edf8d8
serafim <serafim.urukov@gmail.com>
2016-11-27 23:31:40 +0300

    Сформировал генератор задач. Сгенерированы максимально практичные задачи.

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,16 @@
 var express = require('express');
-var bfg = require('bfg')
+var bfg = require('bfg');
+var fs = require('fs');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/random.json', function(req, res, next) {
-    bfg.generate(function(data) {
-            res.json(data);
-        }
-    );
+    r = Math.floor(Math.random()*15+1);
+    var heapUsed = process.memoryUsage().heapUsed;
+    console.log(heapUsed);
+    var obj = JSON.parse(fs.readFileSync('tasks/'+r+'.json', 'utf8'));
+    res.json(obj);
+    next();
 });
 
 module.exports = router;
